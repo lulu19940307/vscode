83c2d2bb64728f4d2337645c077338d68e4a8d3d
lulu19940307 <lulu19940307@163.com>
2018-05-14 16:19:53 +0800

    vscode5.14

---
+++ js/test.js
@@ -0,0 +1,23 @@
+var arr=[];
+window.onload = function(){
+    var div=document.getElementById("div1");
+        div.onclick=function(){
+        alert("hello world");
+    }
+    var btn=document.getElementById("reverse");
+    btn.onclick=function (){
+       var str=document.getElementById("input-txt");
+       var val=str.value;
+        //alert(val.length);
+        //alert(str_test.length);
+        
+        for(var i=val.length-1;i>=0;i--){
+            arr.push(val[i]);
+        }
+        alert(arr.length);
+        var outputtxt=document.getElementById("output-txt");
+        outputtxt.value=arr;
+       
+    };
+    
+}
